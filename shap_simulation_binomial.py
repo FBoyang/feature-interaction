5a9f27ef205ea0f665fd38da4768d6a7db629b86
Nick Liu <nicholas_liu@brown.edu>
2021-07-01 15:53:18 -0400

    generating shap summary plot

--- shap_simulation_binomial.py
+++ shap_simulation_binomial.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 class MMatrix:
     def __init__(self):
         np.random.seed(1)
-        self.N = np.random.randint(1000, 5001)
-        self.M = np.random.randint(10, 101)
+        self.N = 1000 #np.random.randint(1000, 5001)
+        self.M = 20 #np.random.randint(10, 101)
 
         self.infPhenotypeContributionRateArray = np.array([])
         self.unInfPhenotypeContributionRateArray = np.array([])
@@ -26,11 +26,11 @@ class MMatrix:
     def simulatePhenotypeInfinitesimally(self):
         for i in range(self.M):
             self.infPhenotypeContributionRateArray = np.append(
-                self.infPhenotypeContributionRateArray ,np.random.uniform(-1, 1))
+                self.infPhenotypeContributionRateArray,np.random.uniform(-1, 1))
 
-        transposedMatrix = self.infPhenotypeContributionRateArray.transpose()
+        transposed_matrix = self.infPhenotypeContributionRateArray.transpose()
 
-        self.simulateInfPhenotype = np.dot(self.genotype_matrix,transposedMatrix)
+        self.simulateInfPhenotype = np.dot(self.genotype_matrix,transposed_matrix)
 
     def simulatePhenotypeFinitesimally(self):
         for i in range(self.M):
@@ -41,45 +41,46 @@ class MMatrix:
                 self.unInfPhenotypeContributionRateArray = np.append(
                     self.unInfPhenotypeContributionRateArray,0.0)
 
-        transposedMatrix = self.unInfPhenotypeContributionRateArray.transpose()
+        transposed_matrix = self.unInfPhenotypeContributionRateArray.transpose()
 
-        self.simulateUnInfPhenotype = np.dot(self.genotype_matrix,transposedMatrix)
+        self.simulateUnInfPhenotype = np.dot(self.genotype_matrix,transposed_matrix)
 
     def generateModel(self, genotype, phenotype, modelType):
         X = pd.DataFrame(genotype)
 
-        X.columns = ["G" + str(i) for i in range(self.M)]
+        X.columns = ["G" + str(i+1) for i in range(self.M)]
         y = phenotype
 
         model = sk.linear_model.LinearRegression()
         model.fit(X,y)
 
-        #print("List all coefficients")
-        #for i in range(X.shape[1]):
-        #    print("[",i,"]", "=", model.coef_[i].round(4))
+        print("Model coefficients for " + modelType + ":")
+        for i in range(X.shape[1]):
+           print("[G" + str(i+1) + "]", "=", model.coef_[i].round(4))
 
         #1. Partial dependence plotting
-        #X100 = shap.utils.sample(X, 100)
-        #p=shap.plots.partial_dependence("G0", model.predict, X100, ice=False,
+        # X100 = shap.utils.sample(X, 100)
+        # p=shap.plots.partial_dependence("G0", model.predict, X100, ice=False,
         #    model_expected_value=True, feature_expected_value=True, show=False)
 
         #2.1. Compute SHAP values
         background = shap.maskers.Independent(X, max_samples=100)
-        explainer = shap.Explainer(model.predict, background)
+        explainer = shap.Explainer(model=model, masker=background)
         shap_values = explainer(X)
+
+        shap.plots.beeswarm(shap_values, max_display=X.shape[1])
+
         #2.2. Make a standard partial dependence plot at a specific index
         sample_ind = 18
-        #fig, ax = shap.plots.partial_depenedence_plot("G0", model.predict, X,
+        # fig, ax = shap.plots.partial_depenedence_plot("G0", model.predict, X,
         #        model_expected_value = True, feature_expected_value = True,
         #        show=False,ice=False,shap_values=shap_values[sample_ind:
         #        sample_ind+1,:],shap_value_features=X.iloc[sample_ind:
         #        sample_ind+1,:] )
-        #fig.show()
+        # fig.show()
 
         #3. SHAP plot waterfall
-        shap.plots.waterfall(shap_values[sample_ind], max_display=14) #add show=false for plt.savefig to work
-
-        #plt.show(p)
+        shap.plots.waterfall(shap_values[sample_ind], max_display=14, show=False) #add show=false for plt.savefig to work
         plt.savefig("outputs/" + modelType + ".png")
 
 
@@ -89,4 +90,4 @@ if __name__ == "__main__":
     myMatrix.simulatePhenotypeInfinitesimally()
     myMatrix.simulatePhenotypeFinitesimally()
     myMatrix.generateModel(myMatrix.genotype_matrix,myMatrix.simulateInfPhenotype, "infinitesimally")
-    myMatrix.generateModel(myMatrix.genotype_matrix,myMatrix.simulateUnInfPhenotype, "finitesimally")
+    #myMatrix.generateModel(myMatrix.genotype_matrix,myMatrix.simulateUnInfPhenotype, "finitesimally")
