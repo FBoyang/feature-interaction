7795a267deb4934513c4f7580229bafa54f9dd13
Nick Liu <nicholas_liu@brown.edu>
2021-07-08 14:07:18 -0400

    genotype normalization, shap mean abs. value & coefficient comparison,

--- shap_simulation_binomial_noised.py
+++ shap_simulation_binomial_noised.py
@@ -27,9 +27,12 @@ class MMatrix:
         self.genotype_matrix = np.empty((self.N, self.M))
 
         for i in range(0, self.M):
-            p = np.random.uniform(0, 1) #p is fixed column-wise
+            p = np.random.uniform(0, 1)
             for j in range(0, self.N):
                 self.genotype_matrix[j][i] = np.random.binomial(2, p)
+
+        #column-wise normalization of genotype matrix to have mean=0 and variance=1
+        self.genotype_matrix = sk.preprocessing.scale(self.genotype_matrix)
 
     def simulatePhenotypeInfinitesimally(self):
         for i in range(self.M):
@@ -75,6 +78,8 @@ class MMatrix:
         model = sk.linear_model.LinearRegression()
         model.fit(X,y)
 
+        self.coefficients = model.coef_
+
         # print("Model coefficients for " + modelType + ":")
         # for i in range(X.shape[1]):
         #    print("[G" + str(i+1) + "]", "=", model.coef_[i].round(4))
@@ -89,9 +94,12 @@ class MMatrix:
         explainer = shap.Explainer(model=model, masker=background)
         shap_values = explainer(X)
 
-        shap.plots.beeswarm(shap_values, max_display=20, show=False)
-        plt.savefig("outputs/" + modelType + "_summary_" + self.g + "g" + ".png")
-        plt.close()
+
+        self.vals = np.abs(explainer.shap_values(X)).mean(0)
+
+        # shap.plots.beeswarm(shap_values, max_display=20, show=False)
+        # plt.savefig("plots/" + modelType + "_summary_" + ".png")
+        # plt.close()
 
         #2.2. Make a standard partial dependence plot at a specific index
         sample_ind = 18
@@ -103,16 +111,44 @@ class MMatrix:
         # fig.show()
 
         #3. SHAP plot waterfall
-        shap.plots.waterfall(shap_values[sample_ind], max_display=20, show=False)
-        plt.savefig("outputs/" + modelType + "_waterfall_" + self.g + "g" + ".png")
+        # shap.plots.waterfall(shap_values[sample_ind], max_display=20, show=False)
+        # plt.savefig("plots/" + modelType + "_waterfall_" + "18" + ".png")
+        # plt.close()
+
+        #Absolute value bar plot
+        # shap.plots.bar(shap_values, max_display=20)
+        # plt.savefig("plots/" + modelType + "_bar_" + self.g + "g" + ".png")
+        # plt.close()
+
+    def export(self):
+        np.savetxt("outputs/genotype_" + self.g + "g.csv", self.genotype_matrix, delimiter=",")
+        np.savetxt("outputs/phenotype_" + self.g + "g.csv", self.simulate_inf_phenotype_noised)
+
+        combined = np.transpose(np.vstack((self.inf_beta_array, self.coefficients, self.vals)))
+        np.savetxt("outputs/coefficients_and_shap_" + self.g + "g.csv", combined, delimiter=",", header="True Coefficients,sklearn Coefficients,Mean Abs. Shap Values")
+
+
+        #code to generate comparison bar plots
+        combined_abs = np.transpose(np.vstack((np.abs(self.inf_beta_array), np.abs(self.coefficients), self.vals)))
+        headers = ["True Coefficient Absolute Value", "sklearn Coefficient Absolute Value", "SHAP Mean Absolute Value"]
+
+        x = np.arange(combined_abs.shape[0])
+        dx = (np.arange(combined_abs.shape[1]) - combined_abs.shape[1] / 2.) / (combined_abs.shape[1] + 2.)
+        d = 1. / (combined_abs.shape[1] + 2.)
+
+        fig, ax = plt.subplots()
+        for i in range(combined_abs.shape[1]):
+            ax.bar(x + dx[i], combined_abs[:, i], width=d, label=headers[i])
+
+        plt.legend(framealpha=1)
+        plt.savefig("plots/comparison_" + self.g + "g.png", dpi=1000)
         plt.close()
 
 
 if __name__ == "__main__":
-    for i in range (1, 21):
-        myMatrix = MMatrix(i*0.05)
+    for i in range(0, 6):
+        myMatrix = MMatrix(i/10)
         myMatrix.generateMatrix()
         myMatrix.simulatePhenotypeInfinitesimally()
         myMatrix.generateModel(myMatrix.genotype_matrix, myMatrix.simulate_inf_phenotype, "infinitesimally")
-    #myMatrix.simulatePhenotypeFinitesimally()
-    #myMatrix.generateModel(myMatrix.genotype_matrix, myMatrix.simulate_uninf_phenotype, "finitesimally")
+        myMatrix.export()
