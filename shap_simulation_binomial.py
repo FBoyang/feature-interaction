afa65e7f36032f4cb2e18fc643a430bd0e679ca4
phdtrong <70780829+phdtrong@users.noreply.github.com>
2021-07-08 19:43:06 -0700

    Update shap_simulation_binomial.py
    Generate multiple set of genotype and noise variances before revisiting fitted model and exporting to csv files
    Get result of true coefficients, fitted model's coefficients, and SHAP mean values before comparing them as charts

--- shap_simulation_binomial.py
+++ shap_simulation_binomial.py
@@ -1,146 +1,128 @@
 import numpy as np
-import random as rd
 import pandas as pd
 import shap
 import sklearn as sk
 import sympy as sp
 import matplotlib.pyplot as plt
 
-#A/Defining class
 class MMatrix:
     #1.Defining model variables and SHAP variables
-    def __init__(self):
+    def __init__(self, param_g,param_N,param_M):
         #1.1.1.Size of matrices
-        np.random.seed(1)
-        self.N = np.random.randint(1000, 5001)
-        self.M = np.random.randint(10, 101)
+        np.random.seed(10000)
+        self.N = param_N #np.random.randint(1000, 5001)
+        self.M = param_M #np.random.randint(10, 101)
         
         #1.1.2.Variance of coefficients(beta) and noise(epsilon)
-        self.sigma_e_squared = rd.uniform(0,0.5)
-        self.sigma_g_squared = 1-self.sigma_e_squared
+        self.sigma_g_squared = param_g #np.random.uniform(0, 0.5)
+        self.sigma_e_squared = 1 - self.sigma_g_squared
         
         #1.1.3.Distribution of coeeficients(beta) and noise(epsilon)
         self.beta = np.random.normal(0,self.sigma_g_squared/self.M,self.M)
         self.epsi = np.random.normal(0,self.sigma_e_squared,self.N)
-        
+
         #1.1.4.Original model
         self.X = np.empty((self.N, self.M)) # Gene matrix X (N*M)
         self.y = np.empty(self.N) # original Phenotype matrix y (N)
-                
+
         #1.2.1.SHAP modeling
         self.sBeta = np.empty(self.M) # predicted shap coeficiences
-        self.sy = np.empty(self.N) # predicted shap phenotype
+        self.sABSValues = np.empty(self.M) # neutralized absoluted SHAP values
         
         #1.2.2.SHAP parameters
         self.maxDisplayInSHAP = 10 #wanna see maximum 10 features of SHAP
         self.sampleIndex = 1 #test SHAP correctioness at specific sample index
         self.maxSampleSize = 10 #to assign maximum sample size in shap.masker()
-        
-    def mNormalize(self, matrix):
-        #2.1. (1)-D matrix
-        if (len(matrix)==1):
-            norm = np.linalg.norm(matrix)
-            normal_array = matrix/norm
-            return normal_array
-        #2.2. (2+)-D matrix
-        else:
-            return sk.preprocessing.normalize(matrix, axis=1, norm='l1')
-        
+
     #2.Generate a genotype matrix
+    #Credit Henry,Nick    
     def generateMatrix(self):
         #2.1.Generate genotype matrix X
-        for i, col in enumerate(self.X):
-            np.random.seed(1)
-            p = rd.uniform(0,1)
-            for j, row in enumerate(col):
-                self.X[i][j] = np.random.binomial(2, p)
+        for i in range(self.M):
+            p = np.random.uniform(0, 1)
+            for j in range(self.N):
+                self.X[j][i] = np.random.binomial(2, p)
+
+        #2.2.Normalize the genotype matrix column-wisely
+        self.X = sk.preprocessing.scale(self.X)
     
     #3.Simulate a phenotype matrix infinitesimally
     def simulatePhenotypeInf(self):
-        self.y = np.add(np.dot(self.X,self.beta),self.epsi)
-
+        #y = X.b + e
+        self.y = np.add(np.dot(self.X,self.beta.transpose()),self.epsi)
+    
     #4.Fitting X,y to linear model and SHAP-ing the model
     def generateModel(self, genotype, phenotype, modelType):
-        #4.1.Naming each gene in the genotype matrix by SNP orders
+        #4.1.1.Naming each gene in the genotype matrix by SNP orders
         X = pd.DataFrame(genotype)
-        X.columns = ["G" + str(i) for i in range(self.M)]
+        #4.1.2.Naming each SNP
+        X.columns = ["SNP" + str(i+1) for i in range(self.M)]
         y = phenotype
         
         #4.2.1.Fitting X,y to model
         model = sk.linear_model.LinearRegression()
         model.fit(X,y)
-
+        
         #4.2.2.Get all the coef. to an array for later comapre with true coef.
-        for i in range(X.shape[1]):
-            self.sBeta[i] = format(model.coef_[i])
+        self.sBeta = model.coef_
         
         #4.2.3.Compute SHAP values
-        background = shap.maskers.Independent(X, max_samples=self.maxSampleSize)
-        explainer = shap.Explainer(model.predict, background)
+        background = shap.maskers.Independent(X, self.maxSampleSize)
+        explainer = shap.Explainer(model=model.predict, masker=background)
         shap_values = explainer(X)
-        
-        #4.2.4.Get all the phenotype to an array for later compare with true pheno.
-        for i in range(self.N):
-            self.sy[i] = shap_values.base_values[i]
-            for j in range(self.M):
-                self.sy[i] +=self.sBeta[i]*shap_values.data[i][j]
-        
-        #4.3.1.Make a standard partial dependence plot at a specific index
-        sample_ind = self.sampleIndex
-        
-        #4.3.2.SHAP plot waterfall
-        shap.plots.waterfall(shap_values[sample_ind],
-                             max_display=self.maxDisplayInSHAP, show=False)
-        
-        #4.3.3.Save result to a file
-        plt.savefig("waterfall"+modelType+".png")
-    
-    #5.Export data to a file to run R analysis
-    def exportPairDataToFile(self, data1, title1, data2, title2, fileName):
-        #5.1.Store first column of data
-        df = pd.DataFrame(data1)
-        df.columns = [title1]
-        #5.2.Store second column of data
-        df[title2] = pd.DataFrame(data2)
-        print(df)
-        #5.3.Store data frame into a .cvs file
-        df.to_csv(fileName)
-        
-    #6.Export gene data to 2 files
-    def exportGeneDataToFile(self, data1, fileName1, data2, fileName2):
-        #6.1.Store gene data to file 1
-        df = pd.DataFrame(data1)
-        print("X\n",df)
-        #5.2.Store data frame into a .cvs file
-        df.to_csv(fileName1)
-        
-        #6.1.Store gene data to file 2
-        df = pd.DataFrame(data2)
-        print("y\n",df)
-        #5.2.Store data frame into a .cvs file
-        df.to_csv(fileName2)
 
-#B.Executing the defined class
+        #4.2.4.Neutralized absoluted SHAP values with mean at 0
+        self.sABSValues = np.abs(shap_values.values).mean(0)
+    
+    #5.1.Export gene to file
+    def exportFile(self, data, title):
+        #5.1.1.Store data to daframe
+        df = pd.DataFrame(data)
+        #5.1.2.Store data frame into a .cvs file
+        df.to_csv(title+".csv")
+    
+    #5.2.Export {trueCoef,sklearnCoef,shapValues} comparison to charts
+    #Credit: Nick
+    def exportComparisonChart(self,data1,title1,data2,title2,data3,title3):
+        #5.2.1.Collect data in orders
+        mainframe = np.transpose(np.vstack((data1,data2,data3)))
+        self.exportFile(mainframe,"output/"+"["+str(self.N)+"x"+str(self.M)+
+            "]"+title1+"_"+title2+"_"+title3+" at g="+str(self.sigma_g_squared))
+        
+        #5.2.2.Prepare data for charting
+        mainframe = np.transpose(np.vstack((np.abs(data1),np.abs(data2),np.abs(data3))))
+        header = [title1,title2,title3]
+        
+        #5.2.3.Set up scale inside the chart
+        x =np.arange(mainframe.shape[0]) #how many bins to show on plot
+        
+        dx=np.arange(mainframe.shape[1])-mainframe.shape[1]/2 #positions of each column in a bin
+        dx /= mainframe.shape[1]+1 #neutralize as relative positions
+        d = 1/(mainframe.shape[1]+1)#width of each column in a bin
+        
+        #5.2.4.Plotting task
+        fig, ax = plt.subplots()
+        for i in range(mainframe.shape[1]):
+            ax.bar(x+dx[i], mainframe[:,i], width=d, label=header[i])
+        
+        #5.2.5.Store chart
+        plt.legend(framealpha=1)
+        plt.savefig("comparison/"+"["+str(self.N)+"x"+str(self.M)+"]"+
+                    str(self.sigma_g_squared) + "g.png", dpi=1000)
+        plt.close()
+    
 if __name__ == "__main__":
-    #1.Calling matrix object
-    myMatrix = MMatrix()
-    
-    #2.Generate corresponding matrices
-    myMatrix.generateMatrix()
-    
-    #3.Simulate phenotype matrix infinitesimally
-    myMatrix.simulatePhenotypeInf()
-    
-    #4.Fitting dataset (X,y) to linear model and observing result
-    myMatrix.generateModel(myMatrix.X, myMatrix.y, "_infinitesimally")
-    
-    #5.Store paired data of coef_ in both original and SHAP-ed model to file
-    myMatrix.exportPairDataToFile(myMatrix.beta,'trueValue',
-            myMatrix.sBeta,'shapValue','./trueShapPairSamples.csv')
-    
-    #6.Store geno- and pheno- data into files
-    myMatrix.exportGeneDataToFile(myMatrix.X,'genotype.csv',
-                                  myMatrix.y,'phenotype.csv')
-    #6.Store paired data of phenotype of oriniginal and SHAP-ed model to file
-    #myMatrix.exportPairDataToFile(myMatrix.y,
-    #    'trueValue',myMatrix.sy,'shapValue','./trueShapPairSamples.csv')
+    for i in range(0,11):
+        #Define setting
+        myMatrix = MMatrix(i/10,100,70)
+        myMatrix.generateMatrix()
+        myMatrix.simulatePhenotypeInf()
+        myMatrix.generateModel(myMatrix.X, myMatrix.y, "infinitesimally")
+        #EWxport matrices
+        myMatrix.exportFile(myMatrix.X, "output/"+"["+str(myMatrix.N)+"x"+
+            str(myMatrix.M)+"]"+"genotype at g="+str(myMatrix.sigma_g_squared))
+        myMatrix.exportFile(myMatrix.X, "output/"+"phenotype at g="+"["+
+            str(myMatrix.N)+"x"+str(myMatrix.M)+"]"+str(myMatrix.sigma_g_squared))
+        #Export charts
+        myMatrix.exportComparisonChart(myMatrix.beta,"trueCoef",
+            myMatrix.sBeta,"predictedCoef",myMatrix.sABSValues,"meanedSHAPValues")
